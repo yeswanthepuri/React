4b98f539bc079453a0ad983f1b94689c2c55a57e
yeswanthepuri <epuri.yeshu@gmail.com>
2024-09-20 17:18:04 +0530

    Route

--- 07reactroute/src/components/Github.jsx
+++ 07reactroute/src/components/Github.jsx
@@ -1,26 +1,16 @@
 import React from 'react'
 import { useEffect } from 'react'
-import {useLoaderData} from 'react-router-dom'
+import { useLoaderData } from 'react-router-dom'
 
 function Github() {
 
-     const data = useLoaderData()
+    const data = useLoaderData();
 
-    // const [data, setData] = React.useState([])
-    // useEffect(() => {
-    //     fetch('https://api.github.com/users/yeswanthepuri')
-    //     .then((response) => response.json())
-    //     .then(data => {
-    //         console.log(data)
-    //         setData(data)
-    //     })
-    // }, [])
-
-  return (
-    <div className='text-center m-4 bg-gray-600 text-white p-4 text-3xl'>Github followers: {data?.followers}
-    <img src={data?.avatar_url} width={300} alt="" />
-    </div>
-  )
+    return (
+        <div className='text-center m-4 bg-gray-600 text-white p-4 text-3xl'>Github followers: {data?.followers}
+            <img src={data?.avatar_url} width={300} alt="" />
+        </div>
+    )
 }
 
 export default Github
